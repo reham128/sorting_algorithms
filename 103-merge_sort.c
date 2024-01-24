0bb38bfb93cc5da81c168637fe8dffa295639c03
Reham <r_nour30@yahoo.com>
2024-01-24 04:38:30 -0800

    up

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,59 +1,83 @@
 #include "sort.h"
 /**
  * merge_arrays - function to merg the sorted sub arrays
- * */
-void merge_arrays(int *array, size_t lb,size_t m, size_t ub, int * cpy)
+ * @array: is the input array to be sorted
+ * @lb: is the lowe band of subarray
+ * @m: is the middel index of the array
+ * @ub: is the uber band of subarray
+ * @cpy: is the array copy
+ * Return: void function returns nothing
+ */
+void merge_arrays(int *array, size_t lb, size_t m, size_t ub, int *cpy)
 {
 	size_t i = lb;
 	size_t j = m;
-	size_t k;
+	size_t k = lb;
 
-	printf("Merging...\n");
-	printf("[left]: ");
+	printf("Merging...\n[left]: ");
 	print_array(array + lb, m - lb);
 	printf("[right]: ");
 	print_array(array + m, ub - m);
-	for (k = lb; k < ub; k++)
+	while (k < ub)
 	{
 		if (i < m && (j >= ub || array[i] <= array[j]))
 		{
-			cpy[k] = array[i];
-			i++;
+			cpy[k] = array[i++];
 		}
 		else
 		{
-			cpy[k] = array[j];
-			j++;
+			cpy[k] = array[j++];
 		}
+		k++;
 	}
 	printf("[Done]: ");
-	print_array(cpy, ub - lb);
+	print_array(cpy + lb, ub - lb);
 }
 /**
- * */
+ * merge_recursion - function to recrusive divide the array
+ * @array: is the input array to be sorted
+ * @lb: is the lowe band of subarray
+ * @ub: is the uber band of subarray
+ * @cpy: is the array copy
+ * Return: void function returns nothing
+*/
 void merge_recursion(int *array, size_t lb, size_t ub, int *cpy)
 {
 	size_t m;
+
+	if (ub - lb < 2)
+	{
+		return;
+	}
 	if (lb < ub)
 	{
 		m = (lb + ub) / 2;
 		merge_recursion(cpy, lb, m, array);
-		merge_recursion(cpy, m + 1, ub, array);
+		merge_recursion(cpy, m, ub, array);
 		merge_arrays(array, lb, m, ub, cpy);
 	}
 }
 /**
- * */
+ * merge_sort - function to sort an array using merge algorithm
+ * @array: is the input array to be sorted
+ * @size: is the array size
+ * Return: void function returns nothing
+*/
 void merge_sort(int *array, size_t size)
 {
 	int *cpy;
 	size_t i;
 
 	if (array == NULL || size < 2)
+	{
 		return;
+	}
 	cpy = malloc(size * sizeof(int));
 	if (cpy == NULL)
+	{
+		free(cpy);
 		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		cpy[i] = array[i];
